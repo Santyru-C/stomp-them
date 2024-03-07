2877ad086f613546a066bc5b441a2a9ede47c5c5
Santyru <santiago0988@hotmail.com>
2024-03-07 14:30:09 -0300

    add conditional to enable double spawn of mobs

--- main/scenes/SpawnerManager.gd
+++ main/scenes/SpawnerManager.gd
@@ -1,19 +1,36 @@
 extends Node2D
 
+@export var double_spawn_chance : float = 0.2
 var spawner_list
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawner_list = find_child("SpawnerStack").get_children()
 	print(spawner_list.size())
+	
+func calculate_double_spawn() -> bool:
+	var n : float = randf_range(0.0, 1.0)
+	var double : bool = n <= double_spawn_chance
+	
+	return double
 
 func command_random_spawner_for_mob():
-	var n : int = randi_range(0, spawner_list.size() - 1)
-	spawner_list[n].spawn_mob()
+	var rand_spawner_1 = spawner_list.pick_random()
+	var is_double : bool = calculate_double_spawn()
+	
+	rand_spawner_1.spawn_mob()
+	
+	if is_double:
+		print("2 boundle")
+		var remaining_spawners = spawner_list.duplicate()
+		remaining_spawners.erase(rand_spawner_1)
+		var rand_spawner_2 = remaining_spawners.pick_random()
+		
+		rand_spawner_2.spawn_mob() 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
 	
 func _on_spawn_frequency_timer_timeout():
-	print("timer out")
 	command_random_spawner_for_mob()
 	
