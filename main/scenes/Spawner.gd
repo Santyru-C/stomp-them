a8648d4493123df99b62a1a0561906823f2a6825
Santyru <santiago0988@hotmail.com>
2024-03-07 13:08:19 -0300

    center mob position to spawner

--- main/scenes/Spawner.gd
+++ main/scenes/Spawner.gd
@@ -4,8 +4,9 @@ var mob = preload("res://scenes/mob.tscn")
 
 func _ready():
 	print("Running Spawner.gd")
+	spawn_mob()
 	
 func spawn_mob():
 	var mob_instance = mob.instantiate()
 	add_child(mob_instance) # actually the mob instance does not have to be a child of this scene but this will do for now
-
+	mob_instance.position = Vector2(0,0)
